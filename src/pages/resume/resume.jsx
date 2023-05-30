9ec31845cc23e5dd8147ff98411702b08f74b11d
NotAlxT <anhntran56@gmail.com>
2023-05-30 00:17:24 -0400

    commit

--- src/pages/resume/resume.jsx
+++ src/pages/resume/resume.jsx
@@ -1,25 +1,53 @@
 import React from 'react'
+import "./resume.css"
 
 export default function Resume() {
   return (
-    <div id='resume'>
+    <div className="resumeContainer" id='resume'>
       <div className='resumeMainContainer'>
-
-        <section className="resumeContainer">
-          <div className="resumeTitle">
-            <h1>Resume</h1>
-            <p>Description</p>
+        
+        <section className="resumeSubContainer">
+          <h1 className='resumeHeading'>Experience</h1>
+          <div className="resumeTitle1">
+            <h1>Education</h1>
+            <p>Date</p>
+            <p>Title</p>
+            <p>Des</p>
+          </div>
+          <div className="resumeDes1">
+            <p>Description1</p>
           </div>
         </section>
 
-        <section className='skillsContainer'>
+        {/* <section className="resumeSubContainer">
+          <div className="resumeTitle2">
+            <h1>Resume2</h1>
+          </div>
+          <div className="resumeDes2">
+            <p>Description2</p>
+          </div>
+        </section>
+
+        <section className="resumeSubContainer">
+          <div className="resumeTitle3">
+            <h1>Company</h1>
+            <p>Date</p>
+            <p>Title</p>
+            <p>Des</p>
+          </div>
+          <div className="resumeDes3">
+            <p>Description3</p>
+          </div>
+        </section> */}
+
+        {/* <section className='skillsContainer'>
           <div className="skillTitle">
             <h1>Skills</h1>
             <p>Description</p>
           </div>
           <div className="skillItems">
           </div>
-        </section>
+        </section> */}
 
       </div>
     </div>
