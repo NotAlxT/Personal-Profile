4c96ebafd71a9282d32bc32cab14dc9f5f2b2576
NotAlxT <anhntran56@gmail.com>
2023-06-09 12:37:18 -0400

    commit

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
 import './nav.css'
 import LinkedInImage from '../../img/LinkedInImg.png'
 import GitHubImage from '../../img/Github.png'
